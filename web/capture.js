e5abb3c2f138981a9e071e1c6e3672513a4eaca0
asenawritescode <samasena2001@gmail.com>
2023-04-21 11:41:24 +0300

    Remove fetch

--- web/capture.js
+++ web/capture.js
@@ -1,24 +1,13 @@
 document.onmousemove = function (e) {
     // parse to json 
-    let data = {
+    let data1 = {
         "x": e.pageX,
         "y": e.pageY
     };
-
-    // send data to an api 
-    // fetch('http://localhost:3000/data',{
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'            
-    //     },
-    //     body: JSON.stringify(data),
-    // },{mode: 'no-cors'});
-
     // send data to an api using xhr
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:3000/data", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    console.log(data);
-    xhr.send(data);
-    
+    console.log(data1);
+    xhr.send(JSON.stringify(data1));
 };
